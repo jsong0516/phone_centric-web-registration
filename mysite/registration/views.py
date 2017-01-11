7ea43d37a8609260fc75dc99ad315d7c9d92412f
Byung Gon Song <jsong0516@gmail.com>
2017-01-10 20:30:57 -0800

    First index page is done

---
+++ mysite/registration/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render,loader
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index(request):
+	template = loader.get_template('registration/index.html')
+	return HttpResponse(template.render())
+    #return HttpResponse("Hello, world. You're at the Registation index.")
